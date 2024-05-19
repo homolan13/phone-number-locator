8e0f800c3eb0a2a7b5a5231968242d2a57837818
homolan13 <93644446+homolan13@users.noreply.github.com>
2024-05-19 14:54:17 +0200

    Add mkdir if folder for storing HTML files does not exist

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+import os
+
 from colorama import init
 
 from utils import cli_argument, Pipeline
 
 if __name__ == "__main__":
     init()
+
+    if os.path.exists("located_numbers"):
+        os.mkdir("located_numbers")
+
     args = cli_argument()
     phone_number = args.phone_number
     pipeline = Pipeline(args)
